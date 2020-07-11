4fffe750dee92feccc33d33cd296d9a0cc069367
Damon Schulz <damon.schulz@gmail.com>
2020-07-11 17:27:49 -0500

    added widths for columns of data, used jquery to ad head
    git commit
    add .
    git status
    //
    ..

--- app.js
+++ app.js
@@ -4,36 +4,60 @@ $(document).ready(function () {
         dataType: "json",
         type: 'get',
         success: function (data, testStatus, jQxhr) {
-            console.log(data);
-            //visualize data
+            let table = document.getElementById("table");
+            let headers = ["Artist", "Album", "Title", "Genre", "Release Date"];
+            let columnCount = headers.length;
+            let row = table.insertRow(-1);
+
+            for (let i = 0; i < columnCount; i++) {
+                let headerCell = document.createElement("TH");
+                headerCell.innerHTML = headers[i];
+                row.appendChild(headerCell);
+            }
+                $("th:nth-child(1)").addClass("artist");
+                $("th:nth-child(2)").addClass("album");
+                $("th:nth-child(3)").addClass("title");
+                $("th:nth-child(4)").addClass("genre");
+                $("th:nth-child(5)").addClass("releaseDate");
 
             for (let i = 0; i < data.length; i++) {
-                let table = document.getElementById("table");
-                let row = table.insertRow(i);
-                let artist = row.insertCell(0);
-                let album = row.insertCell(1);
-                let title = row.insertCell(2);
-                let genre = row.insertCell(3);
-                let releaseDate = row.insertCell(4);
-                
+
+                row = table.insertRow(i + 1);
+                artist = row.insertCell(0);
+                album = row.insertCell(1);
+                title = row.insertCell(2);
+                genre = row.insertCell(3);
+                releaseDate = row.insertCell(4);
+
                 artist.innerHTML = data[i]["artist"];
                 album.innerHTML = data[i]["album"];
                 title.innerHTML = data[i]["title"];
                 genre.innerHTML = data[i]["genre"];
                 releaseDate.innerHTML = data[i]["releaseDate"];
 
-                jQuery("tr:nth-child(3n)").addClass("classC");
-
-
-                // $("#title").append(`<div>${data[i]["title"]}</div>`);
-                // $("#genre").append(`<div>${data[i]["genre"]}</div>`);
-                // $("#album").append(`<div>${data[i]["album"]}</div>`);
-                // $("#releaseDate").append(`<div>${data[i]["releaseDate"]}</div>`);
-                // $("#artist").append(`<div>${data[i]["artist"]}</div>`);
+                $("td:nth-child(1)").addClass("artist"); // does this need to be inside loop?
+                $("td:nth-child(2)").addClass("album");
+                $("td:nth-child(3)").addClass("title");     //
+                $("td:nth-child(4)").addClass("genre");
+                $("td:nth-child(5)").addClass("releaseDate");
             }
+            $("tr:nth-child(2n)").addClass("shading");
+
         },
         error: function (jQxhr, textStatus, errorThrown) {
             console.log(errorThrown);
         }
     })
 })
+
+/* look up how to call function and modify page in jquery/ajax
+userSelect // drop down menu (artist, album, etc)
+userInput
+resultsData
+for (let i = 0; i < data.length; i++) { // may need to worry about text casing (lower vs upper case)
+  if (data[i][userSelect].lower.contains(userInput.lower)) { "The Beatles" contains "beatles"
+      add data[i] to resultsData
+  }
+}
+return resultsData as new table
+*/
