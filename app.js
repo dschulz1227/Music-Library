f2b34ecf321b36b8c3ed8969d6766338873b0a63
Damon Schulz <damon.schulz@gmail.com>
2020-07-15 13:08:07 -0500

    changed background color, shading color, font style, added border

--- app.js
+++ app.js
@@ -1,10 +1,67 @@
 $(document).ready(function () {
     $.ajax({
+            url: "http://www.devcodecampmusiclibrary.com/api/music",
+            dataType: "json",
+            type: 'get',
+            success: function (data, testStatus, jQxhr) {
+                let table = document.getElementById("table");
+                let headers = ["Artist", "Album", "Title", "Genre", "Release Date"];
+                let columnCount = headers.length;
+                let row = table.insertRow(-1);
+
+                for (let i = 0; i < columnCount; i++) {
+                    let headerCell = document.createElement("th");
+                    headerCell.innerHTML = headers[i];
+                    row.appendChild(headerCell);
+                }
+                $("th:nth-child(1)").addClass("artist");
+                $("th:nth-child(2)").addClass("album");
+                $("th:nth-child(3)").addClass("title");
+                $("th:nth-child(4)").addClass("genre");
+                $("th:nth-child(5)").addClass("releaseDate");
+
+                for (let i = 0; i < data.length; i++) {
+
+                    row = table.insertRow(i + 1);
+                    artist = row.insertCell(0);
+                    album = row.insertCell(1);
+                    title = row.insertCell(2);
+                    genre = row.insertCell(3);
+                    releaseDate = row.insertCell(4);
+
+                    artist.innerHTML = data[i]["artist"];
+                    album.innerHTML = data[i]["album"];
+                    title.innerHTML = data[i]["title"];
+                    genre.innerHTML = data[i]["genre"];
+                    releaseDate.innerHTML = data[i]["releaseDate"];
+
+
+                }
+                $("tr:nth-child(2n)").addClass("shading");
+                $("td:nth-child(1)").addClass("artist");
+                $("td:nth-child(2)").addClass("album");
+                $("td:nth-child(3)").addClass("title");
+                $("td:nth-child(4)").addClass("genre");
+                $("td:nth-child(5)").addClass("releaseDate");
+            },
+            error: function (jQxhr, textStatus, errorThrown) {
+                console.log(errorThrown);
+            }
+         
+        })
+})
+    
+        
+
+
+
+$(document).ready(function () {
+    $.ajax({
         url: "http://www.devcodecampmusiclibrary.com/api/music",
         dataType: "json",
         type: 'get',
         success: function (data, testStatus, jQxhr) {
-            let table = document.getElementById("table");
+            let table = document.getElementById("newTable");
             let headers = ["Artist", "Album", "Title", "Genre", "Release Date"];
             let columnCount = headers.length;
             let row = table.insertRow(-1);
@@ -35,6 +92,8 @@ $(document).ready(function () {
                 genre.innerHTML = data[i]["genre"];
                 releaseDate.innerHTML = data[i]["releaseDate"];
 
+                myFunction();
+
 
             }
             $("tr:nth-child(2n)").addClass("shading");
@@ -59,7 +118,29 @@ function filterData() {
     let selectionDetail = document.getElementById("detailChoice");
     let userDetailSelecttion = selectionDetail.value;
     console.log(userDetailSelecttion);
-    var results = strUser && userDetailSelecttion;
+    var results = strUser + userDetailSelecttion;
     return results;
 
 }
+
+// function myFunction() {
+//     // Declare variables
+//     let input, filter, table, tr, td, i, txtValue;
+//     input = document.getElementById("detailChoice");
+//     filter = input.value.toUpperCase();
+//     table = document.getElementById("Table");
+//     tr = table.getElementsByTagName("tr");
+
+//     // Loop through all table rows, and hide those who don't match the search query
+//     for (i = 0; i < tr.length; i++) {
+//         td = tr[i].getElementsByTagName("td")[0];
+//         if (td) {
+//             txtValue = td.textContent || td.innerText;
+//             if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//                 tr[i].style.display = "";
+//             } else {
+//                 tr[i].style.display = "none";
+//             }
+//         }
+//     }
+// }
